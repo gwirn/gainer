b5378bc8e6fbd6668342ffa800eefef582e6f137
gwirn <71886945+gwirn@users.noreply.github.com>
2024-01-14 19:40:50 +0100

    apply clippy

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,20 @@ use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
 
-/// A script to replace 
+/// A script to replace
 /// parse_git_branch() {
 ///     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 /// }
-/// in .bashrc or .zshrc in the PS1 
-/// 
+/// in .bashrc or .zshrc in the PS1
+///
 /// *zsh example*
-/// PS1='%~'$'%B%F{166}$(/PATH/TO/GAINER)%f%b %B%F{3} %f%b ' 
+/// PS1='%~'$'%B%F{166}$(/PATH/TO/GAINER)%f%b %B%F{3} %f%b '
 /// *bash example*
 /// PS1=$'\w \033[1;31m\]$(/PATH/TO/GAINER) \033[1;34m\] \e[0m '
 ///  
 /// The function searches for a .git/HEAD file to obtain the current branch information.
 /// It intentionally omits any error messages if a required component is not found, so
-/// the function's error does not need to be redirected using '2> /dev/null', and instead simply returns. 
+/// the function's error does not need to be redirected using '2> /dev/null', and instead simply returns.
 ///
 fn main() {
     // get path of the current directory
@@ -36,11 +36,11 @@ fn main() {
                 Ok(content) => content,
                 Err(..) => return,
             };
-            let content_path: &Vec<&str> = &contents.split(" ").collect();
+            let content_path: &Vec<&str> = &contents.split(' ').collect();
             // get filename and remove newline so it can be printed without disturbing PS1
             let branch_name = match Path::new(&content_path[content_path.len() - 1]).file_name() {
                 Some(name) => match name.to_str() {
-                    Some(namestring) => match namestring.strip_suffix("\n") {
+                    Some(namestring) => match namestring.strip_suffix('\n') {
                         Some(final_name) => final_name,
                         None => return,
                     },
@@ -48,7 +48,7 @@ fn main() {
                 },
                 None => return,
             };
-            // print the branch name with indicating branch emoji 
+            // print the branch name with indicating branch emoji
             print!("\u{1F33F} {branch_name}");
             break;
         }
